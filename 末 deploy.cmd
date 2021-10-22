e8024a38827b8dbf37b66719cd78a6f4d653326d
bluesatin <bluesatin@hotmail.co.uk>
2021-10-23 00:57:41 +0100

    Deploy batch file correction

--- 末 deploy.cmd
+++ 末 deploy.cmd
@@ -6,7 +6,7 @@ setlocal
 ECHO [41;m!WARNING![0;m This will build and deploy the app to GitHub pages.
 SET /P CONFIRM=If you want to continue, type 'DEPLOY': 
 IF /I "%CONFIRM%" NEQ "DEPLOY" GOTO END
-:RUN DEPLOY COMMAND
+:RUN
 npm run deploy
 :END
 endlocal
